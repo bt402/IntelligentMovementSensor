b7ad0c24b2e3f134b9d26292768ee7ab644bd07e
bt402 <bt402@greenwich.ac.uk>
2017-05-04 14:52:26 +0100

    Real time detection fixed, the application picks up the peaks every 500 ms and shows how many peaks there were, it picks up even the small ones, so need to try and normalize that

--- app/src/main/java/edu/greenwich/intelligentmovementsensor/BackgroundDetector.java
+++ app/src/main/java/edu/greenwich/intelligentmovementsensor/BackgroundDetector.java
@@ -13,6 +13,8 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -28,6 +30,7 @@ public class BackgroundDetector extends Service implements SensorEventListener{
     ArrayList<Float> zAxisValues = new ArrayList<>();
 
     static final int DELAY = 5000;
+    long lastUpdate;
 
     LocalBroadcastManager broadcaster;
 
@@ -39,6 +42,7 @@ public class BackgroundDetector extends Service implements SensorEventListener{
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer, mSensorManager.SENSOR_DELAY_NORMAL);
         broadcaster = LocalBroadcastManager.getInstance(this);
+        lastUpdate = System.currentTimeMillis();
     }
 
     static final public String RESULT = "edu.greenwich.intelligentmovementsensor.BackgroundDetector.REQUEST_PROCESSED";
@@ -65,20 +69,30 @@ public class BackgroundDetector extends Service implements SensorEventListener{
         yAxisValues.add(accSensorVals[1]); // [1] - Y axis
         zAxisValues.add(accSensorVals[2]); // [2] - Z axis
 
-        new Thread() {
-          public void run() {
-              while (true){
-                  try {
-                      Thread.sleep(DELAY); // delay for 500 ms
-                      checkSpikes(xAxisValues, yAxisValues, zAxisValues);
-                      xAxisValues = new ArrayList<>();
-                      yAxisValues = new ArrayList<>();
-                      zAxisValues = new ArrayList<>();
-                  } catch (Exception e) {
-                  }
-          }
-          }
-        }.start();
+        long curTime = System.currentTimeMillis();
+
+        if ((curTime - lastUpdate) > 500){ // only reads data twice per second
+            lastUpdate = curTime;
+            //System.out.println("Hello every 5 seconds");
+            checkSpikes(xAxisValues, yAxisValues, zAxisValues);
+            xAxisValues = new ArrayList<>();
+            yAxisValues = new ArrayList<>();
+            zAxisValues = new ArrayList<>();
+        }
+
+        /*new Thread() {
+            public void run() {
+                try {
+                    while(true){
+                        Thread.sleep(DELAY); // delay for 500 ms
+                        checkSpikes(xAxisValues, yAxisValues, zAxisValues);
+                        xAxisValues = new ArrayList<>();
+                        yAxisValues = new ArrayList<>();
+                        zAxisValues = new ArrayList<>();
+                    }
+                } catch (Exception e) {}
+            }
+        }.start();*/
     }
 
     static final float ALPHA = 0.25f;
@@ -93,26 +107,46 @@ public class BackgroundDetector extends Service implements SensorEventListener{
     }
 
     private void checkSpikes(ArrayList<Float> x, ArrayList<Float> y, ArrayList<Float> z){
-        String message = "";
+        ArrayList<Float> xSpikes = new ArrayList<>();
+        ArrayList<Float> ySpikes = new ArrayList<>();
+        ArrayList<Float> zSpikes = new ArrayList<>();
+
         for (int i = 0; i < x.size() - 2; i++) {
             if ((x.get(i + 1) - x.get(i)) * (x.get(i + 2) - x.get(i + 1)) <= 0) {
                 //extremeValues.add(accelerometerXVal[i + 1]);
-                System.out.println(x.get(i+1));
-                message += x.get(i+1) + " ";
+                //message += x.get(i+1) + " ";
+                xSpikes.add(x.get(i+1));
             }
             if ((y.get(i + 1) - y.get(i)) * (y.get(i + 2) - y.get(i + 1)) <= 0) {
                 //extremeValues.add(accelerometerXVal[i + 1]);
-                System.out.println(y.get(i+1));
-                message += y.get(i+1) + " ";
+                //message += y.get(i+1) + " ";
+                ySpikes.add(y.get(i+1));
             }
             if ((z.get(i + 1) - z.get(i)) * (z.get(i + 2) - z.get(i + 1)) <= 0) {
                 //extremeValues.add(accelerometerXVal[i + 1]);
-                System.out.println(z.get(i+1));
-                message += z.get(i+1);
+                //message += z.get(i+1);
+                zSpikes.add(z.get(i+1));
             }
-
-            sendResult(message);
         }
+
+        Set<Float> singleSet = new HashSet<Float>();
+        singleSet.addAll(xSpikes);
+        xSpikes.clear();
+        xSpikes.addAll(singleSet);
+
+        singleSet.clear();
+        singleSet.addAll(ySpikes);
+        ySpikes.clear();
+        ySpikes.addAll(singleSet);
+
+        singleSet.clear();
+        singleSet.addAll(zSpikes);
+        zSpikes.clear();
+        zSpikes.addAll(singleSet);
+
+        singleSet.clear(); // save some memory
+
+        sendResult("There was " + xSpikes.size() + " X spikes, " + ySpikes.size() + " Y spikes, " + zSpikes.size() + " Z spikes");
     }
 
 
